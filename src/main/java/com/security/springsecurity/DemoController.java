ed499593c5a567f6780d6ebb95786ac3454d13da
Akshay-Jogarajiya <jogarajiyaakshay@gmail.com>
2025-05-17 22:58:33 +0530

    secure methods using @PreAuthorize annotation

---
+++ src/main/java/com/security/springsecurity/DemoController.java
@@ -0,0 +1,35 @@
+package com.security.springsecurity;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+
+    @GetMapping("/public")
+    public String publicEndpoint() {
+        return "this is public endpoint";
+    }
+
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/user")
+    public String userEndpoint() {
+        return "this is user endpoint";
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/admin")
+    public String adminEndpoint() {
+        return "this is admin endpoint";
+    }
+
+    @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
+    @GetMapping("/shared")
+    public String sharedEndpoint() {
+        return "this is shared endpoint";
+    }
+
+}
