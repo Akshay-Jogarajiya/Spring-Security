ae2b16b96b7c1b058baadc81bc856f1161d1ef1a
Akshay-Jogarajiya <jogarajiyaakshay@gmail.com>
2025-06-23 16:17:59 +0530

    Add multiple user with In memory

--- src/main/java/com/security/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/security/springsecurity/config/SecurityConfig.java
@@ -6,6 +6,10 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -21,5 +25,23 @@ public class SecurityConfig {
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+
+        UserDetails user = User
+                .withDefaultPasswordEncoder()
+                .username("akshay")
+                .password("12345")
+                .build();
+
+        UserDetails admin = User
+                .withDefaultPasswordEncoder()
+                .username("admin")
+                .password("12345")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
     }
 }
