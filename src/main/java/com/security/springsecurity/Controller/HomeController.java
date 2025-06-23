3091aa9a7757b49de605745b4139f74a07fbf902
Akshay-Jogarajiya <jogarajiyaakshay@gmail.com>
2025-06-23 12:01:42 +0530

    create controller and check login

---
+++ src/main/java/com/security/springsecurity/Controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.security.springsecurity.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/home")
+    public String home() {
+        return "Hello...";
+    }
+
+    @GetMapping("/about")
+    public String about() {
+        return "About...";
+    }
+}
